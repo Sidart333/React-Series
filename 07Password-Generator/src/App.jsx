4c77db979c60fabf3a47b9c32e4e759a6f179974
Sidart333 <siddharthsinghpanwar01@gmail.com>
2025-02-07 18:23:54 +0530

    Password Generator Project

---
+++ 07Password-Generator/src/App.jsx
@@ -0,0 +1,27 @@
+import { useCallback, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "@#$%^&*(};>!?/"
+   },
+    [length, numberAllowed, charAllowed, setPassword])
+   
+
+  return (
+    <>
+     <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
